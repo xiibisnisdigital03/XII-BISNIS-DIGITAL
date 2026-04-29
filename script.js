4bdf6ac207fbcf571c69ac9095d12a16b1ef5962
xiibisnisdigital03 <xiibisnisdigital03@gmail.com>
2026-04-30 01:24:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-let slides = document.querySelectorAll(".slide");
-let index = 0;
 
-function changeSlide() {
-    slides[index].classList.remove("active");
-    index = (index + 1) % slides.length;
-    slides[index].classList.add("active");
-}
-
-setInterval(changeSlide, 4000);

4b47a9cb252a42142772631b03064e3d6de70c22
xiibisnisdigital03 <xiibisnisdigital03@gmail.com>
2026-04-29 23:48:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-const toggle = document.getElementById("toggleMode");
 
-toggle.onclick = () => {
-    document.body.classList.toggle("light");
-
-    if (document.body.classList.contains("light")) {
-        toggle.textContent = "☀️";
-    } else {
-        toggle.textContent = "🌙";
-    }
-};
